55fafa5c200967d8ef7dc2a574a61006598f2f89
Abdelhadi-IFKIRNE <abdelhadi.ifkirne@gmail.com>
2023-05-08 17:20:43 +0000

    update ...

---
+++ src/main/java/com/example/alphaadmin/AlphaAdminApplication.java
@@ -0,0 +1,13 @@
+package com.example.alphaadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlphaAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlphaAdminApplication.class, args);
+    }
+
+}
